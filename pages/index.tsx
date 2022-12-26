1d6fbbdf01b3479d25bb191e8fc381c833f6f856
Let <raulalfonzo66@gmail.com>
2022-12-26 07:42:03 -0400

    Change h1 main text

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,15 +40,15 @@ const Home: NextPage = () => {
         <Nav/>
         <section className='min-h-screen w-full bg-no-repeat sticky top-0 z-0 flex flex-col justify-center bg-[url("../public/futureGrid.webp")] bg-[rgb(105,25,255)] bg-[length:105%_109%]' id='home'>          
           <div className='text-center p-6 px-2'> 
-            <h2 className='text-4xl text-white py-1 font-Roboto'>Front-end web developer</h2>
-            <p className='text-md text-white leading-8 font-Roboto'>
+            <h1 className='text-5xl text-white py-1 font-Roboto text-center md:text-7xl md:leading-tight font-bold lg:text-8xl lg:leading-tight'>Let us program <br/> and design <br/> our future </h1>
+            {/* <p className='text-md text-white leading-8 font-Roboto'>
               Freelancer providing services for programming and bringing frontend designs to life.
-            </p>
+            </p> */}
           </div>
-          <div className='mt-4 flex justify-around md:justify-center md:gap-20 text-gray-600'>
+          {/* <div className='mt-4 flex justify-around md:justify-center md:gap-20 text-gray-600'>
             <BsLinkedin className='text-2xl text-white md:text-4xl cursor-pointer'/>
             <BsGithub className='text-2xl text-white md:text-4xl cursor-pointer'/>
-          </div>
+          </div> */}
           <a
             href="#work"
             className=" px-4 py-3 lg:px-5 lg:py-4 border-2 self-center font-Roboto text-white bg-[length:140%] bg-no-repeat bg-[url('../public/button-light.svg')] bg-[position:400%] hover:bg-[position:50%] hover:text-gray-900 transition-all duration-500 lg:text-lg mt-16"
